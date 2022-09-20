4cbef4ae6c42ffcb6b96f16eae98514512b38841
Wongkraiwich Chuenchomphu <35761701+Kentakoong@users.noreply.github.com>
2022-09-20 19:07:13 +0700

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Wongkraiwich Chuenchomphu on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
